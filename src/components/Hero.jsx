f0fdcec35190a44c4c9e8cfa5427eaeb89a5e9c3
Daniel Awolowo <p2575402@my365.dmu.ac.uk>
2024-03-03 05:21:03 +0000

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,100 @@
+import { useState, useEffect } from "react";
+import { motion } from 'framer-motion';
+// import { ArrowRightCircle } from 'react-bootstrap-icons';
+import TrackVisibility from 'react-on-screen';
+
+const Hero = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = ["The Neural Navigators"];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div className="absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5">
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className="text-4xl text-white">
+            Hi, we are{' '}
+            <span className="text-[#915eff] text-4xl">
+              <TrackVisibility>
+                {({ isVisible }) => (
+                  <motion.span
+                    className={isVisible ? "animate__animated animate__fadeIn text-4xl" : "text-4xl"}
+                  >
+                    {text}
+                  </motion.span>
+                )}
+              </TrackVisibility>
+            </span>
+          </h1>
+          <p className="mt-2 text-lg text-white-100">
+            We have created an environment-themed web app which utilises a threat identifier<br />
+            created using an LLM in a JavaScript game,<br />
+            to help identify environmental threats affecting different animals and helping us implement solutions<br />
+            to save them
+          </p>
+        </div>
+      </div>
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-center p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  )
+}
+
+export default Hero;
